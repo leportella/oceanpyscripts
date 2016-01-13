105c18ac1bed7608619bbaffce56c1e04b251473
leportella <leportella@gmail.com>
2016-01-12 22:25:14 -0200

    adding uv2veldir

--- generaltools.py
+++ generaltools.py
@@ -9,7 +9,7 @@ Practical tools for day to day use
 
 import numpy as np
 import csv
-
+from math import *
 
 def FindSimilar(num,arr):
     """ Finds most similiar value to number (num) in an array (arr)
@@ -21,6 +21,27 @@ def FindSimilar(num,arr):
     arrtemp = np.absolute((arr-(num)))
     idd = np.where(arrtemp==min(arrtemp))
     return idd,
+    
+def uv2veldir(u,v):
+    """ 
+    out = uv2veldir(u,v)
+
+    Convert a u and v velocity to velocity and direction information.
+    
+    Based on this explanation:
+    http://wx.gmu.edu/dev/clim301/lectures/wind/wind-uv.html
+    
+    Result:
+    
+    out['vel'] --> velocity
+    out['dir'] --> direction
+    """
+    vel = []; direc=[]
+    for i in range(0,len(u)):
+        vel.append(sqrt((u[i]**2)+(v[i]**2)))
+        direc.append(degrees(atan2(u[i],v[i])))
+    att = {'vel':vel,'dir':direc}
+    return att    
     
 def csv2array(csv,csvdel=';',inline=0):
     """
