92ac3d5e207035c2b660cb1536da30d80605c9fb
leportella <leportella@gmail.com>
2016-03-27 17:12:58 -0300

    quiver corrente

--- DadosCorrente.py
+++ DadosCorrente.py
@@ -91,31 +91,103 @@ for k in range(1,4): #loop pros 3 pontos
     us = [col for col in series if 'u' in col]
     vs = [col for col in series if 'v' in col]
  
-    sts[k]['umean'] = series[us][:].mean(axis=1)
-    sts[k]['vmean'] = series[vs][:].mean(axis=1)
+    sts[k]['u_depthav'] = series[us][:].mean(axis=1)
+    sts[k]['v_depthav'] = series[vs][:].mean(axis=1)
      
-    out2 = uv2veldir(sts[k]['umean'], sts[k]['vmean'])
+    out2 = uv2veldir(sts[k]['u_depthav'], sts[k]['v_depthav'])
      
-    sts[k]['velmean'] = out2['vel']
-    sts[k]['dirmean'] = out2['dir']   
-    sts[k]['velmean_cm'] =  sts[k]['velmean']*100
-     
-############################## PLOT WINDROSE #####################################
-    plotaWindRose(sts[k]['dirmean'],sts[k]['velmean'],maxYlabel=40, maxLeg=0.30, stepLeg=0.1)     
-    plt.savefig(dirOut + 'CurrentRose_ST00' + str(k) + '_DepthAv.png',dpi=200)
+    sts[k]['vel_depthav'] = out2['vel']
+    sts[k]['dir_depthav'] = out2['dir']   
+    sts[k]['vel_depthav_cm'] =  sts[k]['vel_depthav']*100
     
-    plotaWindRose(sts[k]['dirmean'],sts[k]['velmean_cm'],maxYlabel=40, maxLeg=30, stepLeg=5)     
-    plt.savefig(dirOut + 'CurrentRose_ST00' + str(k) + '_DepthAv_CM.png',dpi=200)    
+    uresidual_50h = []    
+    vresidual_50h = []  
+    
+    for i in range(0,len(sts[k]['u_depthav'])-50,50):
+        uresidual_50h.append(np.mean(sts[k]['u_depthav'][i:i+50]))
+        vresidual_50h.append(np.mean(sts[k]['v_depthav'][i:i+50]))
+    
+    sts[k]['uresidual_50h']=uresidual_50h
+    sts[k]['vresidual_50h']=vresidual_50h
+
+
+    ############################### QUIVER CORRENTE #####################################
+    fig, (ax0, ax1) = plt.subplots(nrows=2, sharey=False, sharex=False, figsize=(11, 5))
+    
+    q = ax0.quiver(sts[k]['u_depthav'],sts[k]['v_depthav'],scale=3)
+    p = plt.quiverkey(q,1480,0.05,0.1,"0.1 m/s",coordinates='data',color='k')
+    ax0.axes.get_yaxis().set_visible(False)
+    ax0.axes.get_xaxis().set_visible(False)
+    ax0.set_ylim(-0.02,0.08)
+    ax0.set_xlim(0,len(sts[k]['u_depthav']))
+    ax0.set_title('Vetores de Corrente - ST00' + str(k))
+    
+    ax1.plot(sts[k]['tempo'],sts[k]['vel_depthav'])
+    ax1.grid()
+    ax1.set_ylim(0,0.3)
+    ax1.set_title('Velocidade da Corrente - ST00' + str(k))
+    plt.savefig(dirOut + 'Corrente_Quiver_ST00' + str(k) + '.png',dpi=200)
+
+
+############################### CORRENTE RESIDUAL #####################################
+fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, sharey=False, sharex=False, figsize=(11, 5))
+
+q = ax0.quiver(sts[1]['uresidual_50h'],sts[1]['vresidual_50h'],scale=0.8,width=0.005)
+p = plt.quiverkey(q,29,0.05,0.05,"0.05 m/s",coordinates='data',color='k')
+ax0.axes.get_yaxis().set_visible(False)
+ax0.axes.get_xaxis().set_visible(False)
+ax0.set_ylim(-0.02,0.08)
+ax0.set_xlim(0,len(sts[1]['uresidual_50h']))
+ax0.set_title('Corrente Residual - 50h - ST001',fontsize=12)
+
+q = ax1.quiver(sts[2]['uresidual_50h'],sts[2]['vresidual_50h'],scale=0.8,width=0.005)
+p = plt.quiverkey(q,29,0.05,0.05,"0.05 m/s",coordinates='data',color='k')
+ax1.axes.get_yaxis().set_visible(False)
+ax1.axes.get_xaxis().set_visible(False)
+ax1.set_ylim(-0.02,0.08)
+ax1.set_xlim(0,len(sts[2]['uresidual_50h']))
+ax1.set_title('Corrente Residual - 50h - ST002',fontsize=12)
+
+q = ax2.quiver(sts[3]['uresidual_50h'],sts[3]['vresidual_50h'],scale=0.8,width=0.005)
+p = plt.quiverkey(q,29,0.05,0.05,"0.05 m/s",coordinates='data',color='k')
+ax2.axes.get_yaxis().set_visible(False)
+ax2.axes.get_xaxis().set_visible(True)
+ax2.set_ylim(-0.02,0.08)
+ax2.set_xlim(0,len(sts[3]['uresidual_50h']))
+ax2.set_title('Corrente Residual - 50h - ST003',fontsize=12)
+
+labels = [item.get_text() for item in ax2.get_xticklabels()]
+labels[0] = '0 h'
+labels[1] = '250 h'
+labels[2] = '500 h'
+labels[3] = '750 h'
+labels[4] = '1000 h'
+labels[5] = '1250 h'
+labels[6] = '1500 h'
+
+ax2.set_xticklabels(labels)
+plt.savefig(dirOut + 'Corrente_Residual_Medida_50h.png',dpi=200)
+
+
+
+############################### PLOT WINDROSE ###############################
+
+######
+#    plotaWindRose(sts[k]['dir_depthav'],sts[k]['vel_depthav'],maxYlabel=40, maxLeg=0.30, stepLeg=0.1)     
+#    plt.savefig(dirOut + 'CurrentRose_ST00' + str(k) + '_DepthAv.png',dpi=200)
+#    
+#    plotaWindRose(sts[k]['dir_depthav'],sts[k]['vel_depthav_cm'],maxYlabel=40, maxLeg=30, stepLeg=5)     
+#    plt.savefig(dirOut + 'CurrentRose_ST00' + str(k) + '_DepthAv_CM.png',dpi=200)    
     
 ############################## PLOT HISTOGRAMA ###################################
-#    PercentHistogram(sts[k]['velmean'],binss=30)
+#    PercentHistogram(sts[k]['vel_depthav'],binss=30)
 #    plt.title(u'Histograma de Velocidade da Corrente Medida - ST00' + str(k))
 #    plt.ylabel(u'Percentual')
 #    plt.xlabel(u'Velocidade (m/s)')
 #    plt.grid()
 #    plt.savefig(dirOut + 'Corrente_ST00' + str(k) + '_HistVento.png',dpi=200)
 #    
-#    PercentHistogram(sts[k]['dirmean'],binss=30)
+#    PercentHistogram(sts[k]['dir_depthav'],binss=30)
 #    plt.title(u'Histograma de Direção da Corrente Medida - ST00' + str(k))
 #    plt.ylabel(u'Percentual')
 #    plt.xlabel(u'Graus')
@@ -123,9 +195,22 @@ for k in range(1,4): #loop pros 3 pontos
 #    plt.savefig(dirOut + 'Corrente_ST00' + str(k) + '_HistDirecao.png',dpi=200)
 #
 #    plt.figure()
-#    plt.plot(sts[k]['tempo'],sts[k]['velmean'])
+#    plt.plot(sts[k]['tempo'],sts[k]['vel_depthav'])
 #    plt.title(u'Velocidade da Corrente Medida - ST00' + str(k))
 #    plt.ylabel(u'Velocidade (m/s)')
 #    plt.grid()
 #    plt.savefig(dirOut + 'Corrente_ST00' + str(k) + '_Velocidade.png',dpi=200)
 #    
+    
+#################################################################################
+##                                                                             ##
+##                                  RESIDUAL                                   ##
+##                                                                             ##
+#################################################################################
+
+ 
+
+
+    
+    
+    
